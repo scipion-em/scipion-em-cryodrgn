e8c3b606661b27a417a3c8fcf840eb902aecfa04
Grigory Sharov <sharov.grigory@gmail.com>
2024-01-31 15:22:05 +0000

    add more plots

--- cryodrgn/viewers.py
+++ cryodrgn/viewers.py
@@ -27,7 +27,8 @@
 import os
 from glob import glob
 
-from pyworkflow.protocol.params import LabelParam, EnumParam, BooleanParam
+from pyworkflow.protocol.params import (LabelParam, EnumParam, BooleanParam,
+                                        IntParam)
 from pyworkflow.protocol.executor import StepExecutor
 from pyworkflow.viewer import DESKTOP_TKINTER
 from pwem.viewers import ObjectView, ChimeraView, EmProtocolViewer
@@ -51,19 +52,24 @@ class CryoDrgnViewer(EmProtocolViewer):
         """ Centralize how files are called. """
         if self.protocol.hasMultLatentVars():
             path = glob(self.protocol.getOutputDir("analyze.*/kmeans*"))[0]
+            out = lambda p: os.path.join(path, p)
             self._updateFilenamesDict({
-                'output_umap': os.path.join(path, 'umap.png'),
-                'output_umaphex': os.path.join(path, 'umap_hex.png'),
-                'output_pca': os.path.join(path, 'z_pca.png'),
-                'output_pcahex': os.path.join(path, 'z_pca_hex.png'),
-                'output_notebook': os.path.join(path, '../cryoDRGN_viz.ipynb')
+                'output_umap': out('umap.png'),
+                'output_umaphex': out('umap_hex.png'),
+                'output_pca': out('z_pca.png'),
+                'output_pcahex': out('z_pca_hex.png'),
+                'output_pca_volN': out('../pc%(pc)d/vol_%(id)03d.mrc'),
+                'output_umap_pcN': out('../pc%(pc)d/umap.png'),
+                'output_umap_pcN_traversal': out('../pc%(pc)d/umap_traversal_connected.png'),
+                'output_notebook': out('../cryoDRGN_viz.ipynb')
             })
         else:
             path = glob(self.protocol.getOutputDir("analyze.*"))[0]
+            out = lambda p: os.path.join(path, p)
             self._updateFilenamesDict({
-                'output_hist': os.path.join(path, 'z_hist.png'),
-                'output_dist': os.path.join(path, 'z.png'),
-                'output_notebook': os.path.join(path, 'cryoDRGN_viz.ipynb')
+                'output_hist': out('z_hist.png'),
+                'output_dist': out('z.png'),
+                'output_notebook': out('cryoDRGN_viz.ipynb')
             })
 
     def _defineParams(self, form):
@@ -82,11 +88,36 @@ class CryoDrgnViewer(EmProtocolViewer):
         if self.protocol.hasMultLatentVars():
             form.addParam('useHexBin', BooleanParam, default=False,
                           label="Use hexagonal bins for the plots?")
-            form.addParam('doShowPCA', LabelParam,
-                          label='Show PCA projection of latent space encodings')
+
+            group = form.addGroup('PCA & UMAP')
+            group.addParam('doShowPCA', LabelParam,
+                           label='Show PCA projection of latent space encodings')
+            group.addParam('pcNum', IntParam, default=1,
+                           label="Which PC to plot",
+                           help="The plots within the pcX subdirectories show the UMAP "
+                                "embedding colored by each particle's projected value "
+                                "along PCX. This helps give a sense of the layout of the "
+                                "latent space and how the UMAP embedding (e.g. a nonlinear "
+                                "8D → 2D embedding) is related to the PCA projection (a "
+                                "linear projection from 8D → 2D).")
+            group.addParam('displayVolPCA', EnumParam,
+                           choices=['slices', 'chimera'],
+                           default=VOLUME_SLICES,
+                           display=EnumParam.DISPLAY_HLIST,
+                           label='Display PCX volumes with',
+                           help="By default, the 10 volumes are generated at equally spaced "
+                                "points between the first and 99th percentile of the data "
+                                "distribution projected onto each principal component.")
+
             if not self.protocol.skipUmap:
-                form.addParam('doShowUMAP', LabelParam,
-                              label="Show UMAP visualization of latent space encodings")
+                group.addParam('doShowUMAP', LabelParam,
+                               label="Show UMAP visualization of latent space encodings")
+
+            group.addParam('doShowPcaToUmap', LabelParam,
+                           label="Show UMAP embeddings colored by PCX value")
+            group.addParam('doShowPcaToUmapTrav', LabelParam,
+                           label="Show UMAP connected traversal along PCX")
+
         else:
             form.addParam('doShowDistr', LabelParam,
                           label='Show latent coordinates distribution')
@@ -110,14 +141,17 @@ class CryoDrgnViewer(EmProtocolViewer):
         self._createFilenameTemplates()
 
         visDict = {
-            'displayVol': self._showVolumes,
+            'displayVol': lambda paramName: self._showVolumes(pca=False),
             'doShowNotebook': self._showNotebook
         }
 
         if self.protocol.hasMultLatentVars():
             visDict.update({
+                'displayVolPCA': lambda paramName: self._showVolumes(pca=True),
                 'doShowPCA': self._showPCA,
-                'doShowUMAP': self._showUMAP
+                'doShowUMAP': self._showUMAP,
+                'doShowPcaToUmap': self._showPCAToUMAP,
+                'doShowPcaToUmapTrav': self._showPCAToUMAPTrav
             })
         else:
             visDict.update({
@@ -127,23 +161,24 @@ class CryoDrgnViewer(EmProtocolViewer):
 
         return visDict
 
-    def _showVolumes(self, paramName=None):
+    def _showVolumes(self, pca=False):
         try:
             if self.displayVol == VOLUME_CHIMERA:
-                return self._showVolumesChimera()
+                return self._showVolumesChimera(pca)
             elif self.displayVol == VOLUME_SLICES:
-                return self._showVolumeSlices()
+                return self._showVolumeSlices(pca)
         except Exception as e:
             self.showError(str(e))
 
-    def _showVolumesChimera(self):
+    def _showVolumesChimera(self, pca=False):
         """ Create a chimera script to visualize selected volumes. """
         prot = self.protocol
+        vols = self._getVolumesNamesPCA() if pca else prot.Volumes.getFiles()
         extra = prot._getExtraPath()
         cmdFile = prot._getExtraPath('chimera_volumes.cxc')
 
         with open(cmdFile, 'w+') as f:
-            for vol in prot.getFiles():
+            for vol in vols:
                 localVol = os.path.relpath(vol, extra)
                 if os.path.exists(vol):
                     f.write(f"open {localVol}\n")
@@ -153,15 +188,36 @@ class CryoDrgnViewer(EmProtocolViewer):
 
         return [view]
 
-    def _showVolumeSlices(self):
+    def _showVolumeSlices(self, pca=False):
         """ Open a sqlite with all volumes selected for visualization. """
-        path = self.protocol._getExtraPath('volumes.sqlite')
+        if pca:
+            path = self.protocol._getExtraPath(f"volumes_pc{self.pcNum}.sqlite")
+            files = self._getVolumesNamesPCA()
+            samplingRate = self.protocol._getOutputSampling()
+            self.createVolumesSqlite(files, path, samplingRate)
+        else:
+            path = self.protocol._getExtraPath('volumes.sqlite')
+
         return [ObjectView(self._project, self.protocol.strId(), path)]
 
-    def _showPlot(self, fn):
+    def _getVolumesNamesPCA(self):
+        """ Get filenames for 10 output volumes along PCX. """
+        names = []
+        vols = [self._getFileName('output_pca_volN',
+                                  pc=self.pcNum,
+                                  id=i) for i in range(10)]
+        for fn in vols:
+            if os.path.exists(fn):
+                names.append(fn)
+            else:
+                raise FileNotFoundError(f"File {fn} not found!")
+
+        return names
+
+    def _showPlot(self, fn, **kwargs):
         import matplotlib.image as mpimg
         import matplotlib.pyplot as plt
-        fn = self._getFileName(fn)
+        fn = self._getFileName(fn, **kwargs)
         if os.path.exists(fn):
             img = mpimg.imread(fn)
             imgplot = plt.imshow(img)
@@ -169,7 +225,7 @@ class CryoDrgnViewer(EmProtocolViewer):
             plt.show()
             return [imgplot]
         else:
-            self.showError(f"File {fn} not found! Have you run analysis?")
+            self.showError(f"File {fn} not found!")
 
     def _showHistogram(self, paramName=None):
         self._showPlot('output_hist')
@@ -184,6 +240,12 @@ class CryoDrgnViewer(EmProtocolViewer):
     def _showUMAP(self, paramName=None):
         fn = 'output_umaphex' if self.useHexBin else 'output_umap'
         self._showPlot(fn)
+
+    def _showPCAToUMAP(self, paramName=None):
+        self._showPlot('output_umap_pcN', pc=self.pcNum)
+
+    def _showPCAToUMAPTrav(self, paramName=None):
+        self._showPlot('output_umap_pcN_traversal', pc=self.pcNum)
 
     def _showNotebook(self, paramName=None):
         """ Open jupyter notebook with results in a browser. """
@@ -208,7 +270,7 @@ class CryoDrgnViewer(EmProtocolViewer):
                                      env=Plugin.getEnviron(),
                                      cwd=fnDir)
             else:
-                self.showError('Jupyter notebook not found! Have you run analysis?')
+                self.showError(f"Jupyter notebook {fn} not found!")
 
         from threading import Thread
         thread = Thread(target=_extraWork)
